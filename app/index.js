08b6acfc3300c843400b18f48de9f32ce964d326
Adrian Puscasu <adi.puscasu@gmail.com>
2017-12-29 01:42:45 +0200

    added more functions

--- app/index.js
+++ app/index.js
@@ -18,23 +18,40 @@ function initGame(cellArray) {
     }
 }
 window.onload = function () {
+    var elmButton = document.getElementById("startGame");
     var drawingSurface = document.getElementById("drawingSurface");
     var elemLeft = drawingSurface.offsetLeft;
     var elemTop = drawingSurface.offsetTop;
     var ctxt = drawingSurface.getContext("2d");
     var isGameStarted = false;
+    var noOfGenerations = 0;
+    var myArray = new Array();
+    initGame(myArray);
+    drawGame(myArray);
+    if (elmButton) {
+        elmButton.addEventListener("click", function (event) {
+            isGameStarted = true;
+            console.log("isGameStarted is set to true");
+            iterationStarted();
+        });
+    }
     drawingSurface.addEventListener("click", function (event) {
         var x = event.pageX - elemLeft, y = event.pageY - elemTop;
         console.log("clicked at: ", x, y);
         setCell({ coordX: Math.floor(x / 4) * 4, coordY: Math.floor(y / 4) * 4 });
     });
-    var myArray = new Array();
-    initGame(myArray);
-    drawGame();
-    function drawGame() {
-        myArray.forEach(function (cell) {
-            ctxt.fillStyle = cell.isAlive ? "#000000" : "#FFFFFF";
-            ctxt.fillRect(cell.cellCoords.coordX, cell.cellCoords.coordY, cell.cellWidth, cell.cellWidth);
+    function drawGame(drawArray) {
+        if (!drawArray) {
+            return;
+        }
+        drawArray.forEach(function (cell) {
+            if (ctxt) {
+                if (!cell) {
+                    return;
+                }
+                ctxt.fillStyle = cell.isAlive ? "#000000" : "#FFFFFF";
+                ctxt.fillRect(cell.cellCoords.coordX, cell.cellCoords.coordY, cell.cellWidth, cell.cellWidth);
+            }
         });
     }
     function setCell(coords) {
@@ -42,13 +59,16 @@ window.onload = function () {
             if (cell.cellCoords.coordX === coords.coordX && cell.cellCoords.coordY === coords.coordY) {
                 cell.isAlive = !cell.isAlive;
                 console.log(cell.isAlive);
-                drawGame();
             }
         });
+        drawGame(myArray);
     }
     function getCellNeighbours(cell) {
         var liveCellsCount = 0;
         myArray.forEach(function (element) {
+            if (!element) {
+                return;
+            }
             switch (element.cellCoords) {
                 case { coordX: cell.cellCoords.coordX, coordY: cell.cellCoords.coordY - 4 }:
                     liveCellsCount++;
@@ -78,7 +98,43 @@ window.onload = function () {
                     liveCellsCount = 0;
                     break;
             }
-            return liveCellsCount;
         });
+        return liveCellsCount;
+    }
+    function iterationStarted() {
+        while (isGameStarted === true) {
+            console.log(myArray);
+            console.log("myArray in iterations started");
+            myArray = getNextGeneration();
+            drawGame(myArray);
+            noOfGenerations++;
+            writeIterationsNumber(noOfGenerations);
+        }
+    }
+    function getNextGeneration() {
+        var newGeneration = myArray.slice(0).map(function (cellEllement) {
+            if (!cellEllement) {
+                return;
+            }
+            var neighbours = getCellNeighbours(cellEllement);
+            switch (neighbours) {
+                case 2:
+                    cellEllement.isAlive = cellEllement.isAlive;
+                    break;
+                case 3:
+                    cellEllement.isAlive = true;
+                    break;
+                default:
+                    cellEllement.isAlive = false;
+                    break;
+            }
+        });
+        return newGeneration;
+    }
+    function writeIterationsNumber(iterationsNumber) {
+        var elmLabel = document.getElementById("iterationsNumber");
+        if (elmLabel) {
+            elmLabel.innerHTML = "Number of iterations : " + iterationsNumber;
+        }
     }
 };
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ class Cell {
     isAlive: boolean;
     cellWidth: number;
     cellCoords: Coords;
-    constructor(alive: boolean, cellSize: number, myCoords: Coords){
+    constructor(alive: boolean, cellSize: number, myCoords: Coords) {
         this.isAlive = alive;
         this.cellWidth = cellSize;
         this.cellCoords = myCoords;
@@ -14,7 +14,7 @@ interface Coords {
     coordY: number;
 }
 
-function initGame(cellArray: Array<Cell>){
+function initGame(cellArray: Array<Cell>) {
     let i = 0;
     while (i < 400){
         let j = 0;
@@ -27,6 +27,7 @@ function initGame(cellArray: Array<Cell>){
 }
 
 window.onload = function () {
+    let elmButton = document.getElementById("startGame");
     let drawingSurface = <HTMLCanvasElement> document.getElementById("drawingSurface");
     let elemLeft = drawingSurface.offsetLeft;
     let elemTop = drawingSurface.offsetTop;
@@ -34,19 +35,33 @@ window.onload = function () {
     let isGameStarted : boolean = false;
     let noOfGenerations : number = 0;
 
+    let myArray = new Array();
+    initGame(myArray);
+    drawGame(myArray);
+
+    if (elmButton) {
+        elmButton.addEventListener("click", function(event){
+            isGameStarted = true;
+            console.log("isGameStarted is set to true");
+            iterationStarted();
+        });
+    }
     drawingSurface.addEventListener("click", function(event){
         let x = event.pageX - elemLeft,
             y = event.pageY - elemTop;
-        console.log("clicked at: ", x,y);
+        console.log("clicked at: ", x, y);
         setCell({coordX: Math.floor(x / 4) * 4, coordY: Math.floor(y / 4) * 4});
     });
-    let myArray = new Array();
-    initGame(myArray);
-    drawGame();
 
-    function drawGame(){
-        myArray.forEach(function(cell){
+    function drawGame(drawArray : Array<Cell>) {
+        if (!drawArray) {
+            return;
+        }
+        drawArray.forEach(function(cell){
             if (ctxt){
+                if (!cell) {
+                    return;
+                }
                 ctxt.fillStyle = cell.isAlive ? "#000000" : "#FFFFFF";
                 ctxt.fillRect(cell.cellCoords.coordX, cell.cellCoords.coordY, cell.cellWidth, cell.cellWidth);
             }
@@ -58,14 +73,17 @@ window.onload = function () {
             if (cell.cellCoords.coordX === coords.coordX && cell.cellCoords.coordY === coords.coordY) {
                 cell.isAlive = !cell.isAlive;
                 console.log(cell.isAlive);
-                drawGame();
             }
         });
+        drawGame(myArray);
     }
 
-    function getCellNeighbours (cell : Cell) {
+    function getCellNeighbours (cell : Cell) : number  {
         let liveCellsCount : number = 0;
         myArray.forEach(element => {
+            if (!element) {
+                return;
+            }
             switch (element.cellCoords) {
                 case {coordX: cell.cellCoords.coordX, coordY : cell.cellCoords.coordY - 4}:
                     liveCellsCount ++;
@@ -95,8 +113,48 @@ window.onload = function () {
                     liveCellsCount = 0;
                     break;
             }
-            return liveCellsCount;
+            
         });
+        return liveCellsCount;
+    }
+
+    function iterationStarted(){
+        while (isGameStarted === true) {
+            console.log(myArray);
+            console.log("myArray in iterations started");
+            myArray = getNextGeneration();
+            drawGame(myArray);
+            noOfGenerations ++;
+            writeIterationsNumber(noOfGenerations);
+        }
+    }
+
+    function getNextGeneration (){
+        let newGeneration = myArray.slice(0).map( function (cellEllement){
+            if (!cellEllement) {
+                return;
+            }
+            const neighbours : number = getCellNeighbours(cellEllement);
+            switch (neighbours) {
+                case 2:
+                    cellEllement.isAlive = cellEllement.isAlive;
+                    break;
+                case 3:
+                    cellEllement.isAlive = true;
+                    break;
+                default:
+                    cellEllement.isAlive = false;
+                    break;
+            }
+        });
+        return newGeneration;
+    }
+
+    function writeIterationsNumber (iterationsNumber : number) {
+        const elmLabel = document.getElementById("iterationsNumber");
+        if (elmLabel) {
+            elmLabel.innerHTML = "Number of iterations : " + iterationsNumber;
+        }
     }
 
 };
